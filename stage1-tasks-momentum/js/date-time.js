65875fb0ea76848626f3eba20381e5b25326d047
Olga Berezka <olgakoltsova@Air-Olga.lan>
2023-02-13 13:25:30 +0000

    feat: add time js

---
+++ stage1-tasks-momentum/js/date-time.js
@@ -0,0 +1,9 @@
+const time = document.querySelector('.time')
+
+function showTime() {
+	const date = new Date()
+	const currentTime = date.toLocaleTimeString()
+	time.textContent = currentTime
+	setTimeout(showTime, 1000)
+}
+showTime()
