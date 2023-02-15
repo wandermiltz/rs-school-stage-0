af5a89089f67c0eee1a187f9d20c4308b5fdd1db
Olga Berezka <olgakoltsova@Air-Olga.lan>
2023-02-15 18:36:53 +0000

    feat: add background img slider

--- stage1-tasks-momentum/js/background-img.js
+++ stage1-tasks-momentum/js/background-img.js
@@ -1,18 +1,43 @@
 import { getTimeOfDay } from './date-time.js'
 
 const body = document.querySelector('body')
+const slideNext = document.querySelector('.slide-next')
+const slidePrev = document.querySelector('.slide-prev')
 
 function getRandomNum(max, min) {
-	let rand = min + Math.random() * (max + 1 - min)
-	return Math.floor(rand)
+	return Math.floor(min + Math.random() * (max + 1 - min))
 }
+
+let randomNum = getRandomNum(1, 20)
 
 function setBg() {
 	let timeOfDay = getTimeOfDay().toLowerCase()
-	let bgNum = getRandomNum(1, 20).toString().padStart(2, '0')
-	console.log(bgNum)
+	let bgNum = randomNum.toString().padStart(2, '0')
 
 	body.style.backgroundImage = `url('https://raw.githubusercontent.com/wandermiltz/stage1-tasks/assets/images/${timeOfDay}/${bgNum}.jpg')`
 }
 
 setBg()
+
+function getSlideNext() {
+
+	if (randomNum < 20) {
+		randomNum += 1
+	} else {
+		randomNum = 1
+	}
+	setBg()
+}
+
+function getSlidePrev() {
+
+	if (randomNum > 1) {
+		randomNum -= 1
+	} else {
+		randomNum = 20
+	}
+	setBg()
+}
+
+slideNext.addEventListener('click', getSlideNext)
+slidePrev.addEventListener('click', getSlidePrev)
