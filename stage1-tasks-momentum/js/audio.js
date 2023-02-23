8b7064c86ff4a8dacddee3b2eaa8d60322664eff
Olga Berezka <olgakoltsova@Air-Olga.lan>
2023-02-23 15:45:06 +0000

    feat: add play btns near tracks

--- stage1-tasks-momentum/js/audio.js
+++ stage1-tasks-momentum/js/audio.js
@@ -26,22 +26,33 @@ function getPlayListItemActive() {
 	if (isPlay) {
 		playListItems.forEach(el => {
 			el.classList.remove('item-active')
+			el.querySelector('.player-icon-small').classList.remove('pause')
 		})
 		playListItems[playNum].classList.add('item-active')
+		playListItems[playNum].querySelector('.player-icon-small').classList.add('pause')
 	}
 }
 
 
+
+playListItems.forEach(el => {
+	const btn = document.createElement('button')
+	btn.classList.add('play')
+	btn.classList.add('player-icon')
+	btn.classList.add('player-icon-small')
+	el.appendChild(btn)
+})
+
 const audio = new Audio()
 let playNum = 0
 let isPlay = false
+audio.volume = 0.25
 
 function playAudio() {
 	audio.src = playList[playNum].src
 	currentSongName.textContent = playList[playNum].title
 	songDuration.textContent = playList[playNum].duration
 	audio.currentTime = 0
-	audio.autoplay = true
 	audio.play()
 	isPlay = true
 }
@@ -66,8 +77,11 @@ function toggleAudio() {
 function toggleAudioIcons() {
 	if (!isPlay) {
 		playBtn.classList.remove('pause')
+		playListItems[playNum].querySelector('.player-icon-small').classList.remove('pause')
 	} else {
 		playBtn.classList.add('pause')
+		playListItems[playNum].querySelector('.player-icon-small').classList.add('pause')
+
 	}
 }
 
@@ -117,6 +131,7 @@ playPrevBtn.addEventListener('click', getAudioPrev)
 soundBtn.addEventListener('click', toggleMute)
 soundBtn.addEventListener('click', toggleMuteIcons)
 
+
 timeline.addEventListener('click', el => {
 	const timelineWidth = window.getComputedStyle(timeline).width
 	const timeToSeek = el.offsetX / parseInt(timelineWidth) * audio.duration
@@ -147,3 +162,30 @@ volumeSlider.addEventListener('click', el => {
 	audio.volume = newVolume
 	volumePercentage.style.width = newVolume * 100 + '%';
 }, false)
+
+
+
+playListItems.forEach((el, i) => {
+
+	el.addEventListener('click', e => {
+		playNum = i
+
+		if (!el.classList.contains('item-active')) {
+			isPlay = false
+			const smallPlayBtns = document.querySelectorAll('.player-icon-small')
+			smallPlayBtns.forEach(el => {
+				el.classList.remove('pause')
+			})
+		}
+
+		getAudioPlayToggle()
+
+		const smallPlayBtn = el.querySelector('.player-icon-small')
+
+		if (!isPlay) {
+			smallPlayBtn.classList.remove('pause')
+		} else {
+			smallPlayBtn.classList.add('pause')
+		}
+	})
+})
